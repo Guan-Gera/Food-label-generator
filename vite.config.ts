167b77a2d9bec9a0492ef1b1ebbb920f6775bc4d
Guan-Gera <122550415+Guan-Gera@users.noreply.github.com>
2025-12-05 23:37:49 +0800

    微调一下 vite.config.ts 和 index.html，确保它们能配合这个生产过程。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // 加载环境变量，确保 Netlify 后台配置的 API_KEY 能被读取
+  const env = loadEnv(mode, (process as any).cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      // 这里的配置让代码里的 process.env.API_KEY 能在浏览器中正常工作
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    }
+  }
 })
